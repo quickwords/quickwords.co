baf77b3b8b73ebd32822be26e838bc4188cacaac
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-03-12 18:44:01 +0100

    Using Tailwind CSS

--- tailwind.js
+++ tailwind.js
@@ -47,13 +47,13 @@ let colors = {
   'transparent': 'transparent',
 
   'black': '#22292f',
-  'grey-darkest': '#3d4852',
-  'grey-darker': '#606f7b',
-  'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
-  'grey-light': '#dae1e7',
-  'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
+  'grey-lightest' : '#FEFFFF',
+  'grey-lighter' : '#FBFDFF',
+  'grey-light' : '#F9FCFE',
+  'grey' : '#F6FBFE',
+  'grey-dark' : '#f1f1f1',
+  'grey-darker' : '#DDE2E5',
+  'grey-darkest' : '#949798',
   'white': '#ffffff',
 
   'red-darkest': '#3b0d0c',
@@ -96,13 +96,13 @@ let colors = {
   'teal-lighter': '#a0f0ed',
   'teal-lightest': '#e8fffe',
 
-  'blue-darkest': '#12283a',
-  'blue-darker': '#1c3d5a',
-  'blue-dark': '#2779bd',
-  'blue': '#3490dc',
-  'blue-light': '#6cb2eb',
-  'blue-lighter': '#bcdefa',
-  'blue-lightest': '#eff8ff',
+  'blue-lightest' : '#EDF7FC',
+  'blue-lighter' : '#B5DDF4',
+  'blue-light' : '#7EC4EB',
+  'blue' : '#46ABE3',
+  'blue-dark' : '#3F9ACC',
+  'blue-darker' : '#2A6788',
+  'blue-darkest' : '#153344',
 
   'indigo-darkest': '#191e38',
   'indigo-darker': '#2f365f',
@@ -169,8 +169,8 @@ module.exports = {
   screens: {
     'sm': '576px',
     'md': '768px',
-    'lg': '992px',
-    'xl': '1200px',
+    // 'lg': '992px',
+    'xl': '1000px',
   },
 
 
@@ -381,7 +381,9 @@ module.exports = {
     default: '1px',
     '0': '0',
     '2': '2px',
+    '3': '3px',
     '4': '4px',
+    '5': '5px',
     '8': '8px',
   },
 
@@ -467,6 +469,7 @@ module.exports = {
     '32': '8rem',
     '48': '12rem',
     '64': '16rem',
+    '80': '20rem',
     '1/2': '50%',
     '1/3': '33.33333%',
     '2/3': '66.66667%',
@@ -505,6 +508,7 @@ module.exports = {
     '2': '0.5rem',
     '3': '0.75rem',
     '4': '1rem',
+    '5': '1.25rem',
     '6': '1.5rem',
     '8': '2rem',
     '10': '2.5rem',
@@ -633,6 +637,7 @@ module.exports = {
     '4': '1rem',
     '6': '1.5rem',
     '8': '2rem',
+    '16': '4rem',
   },
 
 
@@ -661,6 +666,8 @@ module.exports = {
     '4': '1rem',
     '6': '1.5rem',
     '8': '2rem',
+    '16': '4rem',
+    '32': '8rem',
   },
 
 
@@ -712,6 +719,7 @@ module.exports = {
     'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
     'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
     'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
+    'special': '1px 3px 19px 1px #f6fbfe',
     'none': 'none',
   },
 
@@ -840,7 +848,7 @@ module.exports = {
     minHeight: ['responsive'],
     minWidth: ['responsive'],
     negativeMargin: ['responsive'],
-    opacity: ['responsive'],
+    opacity: ['responsive', 'hover'],
     overflow: ['responsive'],
     padding: ['responsive'],
     pointerEvents: ['responsive'],
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,65 @@
 const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
+const glob = require('glob-all')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
 
-mix.js('src/js/app.js', './dist/js/app.js')
-    .sass('src/sass/app.sass', './dist/css/app.css')
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g) || [];
+    }
+}
+
+// .setPublicPath('./dist')
+mix
+    .js('src/js/app.js', 'dist/js/app.js')
+    .sass('src/sass/app.sass', 'dist/css/app.css')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
     })
-    // .version()
     .copyDirectory('src/assets', 'dist/assets')
     .copy('src/index.html', 'dist/index.html')
-    .browserSync('http://quickwords.co.dev/')
+
+if (! mix.config.production) {
+    mix.browserSync({
+        proxy: 'quickwords.co.dev',
+        files: [
+            'src/*.html',
+            'src/*.js',
+            'dist/css/app.css'
+        ]
+    })
+}
+
+// if (mix.config.production) {
+//     mix.version()
+// }
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.config.production) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, 'src/*.html')
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ['html', 'js', 'php', 'vue']
+                    }
+                ]
+            })
+        ]
+    })
+}
